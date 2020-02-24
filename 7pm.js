207444302e0902f87666d0009336e567f703be43
ardit bijwaard <ardittristan@gmail.com>
2020-02-24 23:59:54 +0100

    add comment

---
+++ 7pm.js
@@ -0,0 +1,34 @@
+const Webhooks = require('node-webhooks');
+const GetNextDate = require("get-next-date");
+const GetMidnighDate = require("get-midnight-date");
+
+var webHooks = new Webhooks({
+    db: "./webHook.json",
+    httpSuccessCodes: [204]
+});
+
+//timer
+async function timer() {
+    var now = new Date(Date.now());
+    var nextThursday = new Date(Date.now());
+    if (now.getDay() === 4) {
+        var today7 = GetMidnighDate(now).getTime() + 72000000;
+        if (now.getTime() <= today7) {
+
+        }
+    } else {
+        var running = true;
+        while (running) {
+            nextThursday = GetNextDate(nextThursday);
+            if (nextThursday.getDay() === 4) {
+                running = false;
+            }
+        }
+    }
+    var minutesUntilNextThursday = Math.ceil((nextThursday.getTime() + 72000000 - now.getTime()) / 1000 / 60);
+    console.log(`send ${minutesUntilNextThursday}`);
+    webHooks.trigger("sendTime", { "content": `${minutesUntilNextThursday} minutes until DnD` });
+}
+
+timer();
+setInterval(timer, 60000);
